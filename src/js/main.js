2ddb5c1889cb2b585622dfd518c465b81ce65e02
Thomas Powell <tkpowell@iastate.edu>
2020-02-26 22:53:36 -0600

    Added localStorage to save values

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,9 @@
-let race = "";
-let subRace = "";
-let chtrClass = "";
-let background = "";
-let level = 1;
+var race = "";
+var subRace = "";
+var chtrClass = "";
+var background = "";
+var level = 1;
+var proficiencies = "";
 
 function chooseSubrace() {
     race = document.getElementById("race").value;
@@ -23,17 +24,72 @@ function checkAttributes(){
     background = document.getElementById("background").value;
 
     subRace = "";
-    if(race == "dwarf"){ subrace = document.getElementById("subDwarf").value;}
-    else if(race == "elf"){ subrace = document.getElementById("subElf").value;}
-    else if(race == "halfling"){ subrace = document.getElementById("subHalfling").value;}
-    else if(race == "gnome"){ subrace = document.getElementById("subGnome").value;}
+    if(race == "dwarf"){ subRace = document.getElementById("subDwarf").value;}
+    else if(race == "elf"){ subRace = document.getElementById("subElf").value;}
+    else if(race == "halfling"){ subRace = document.getElementById("subHalfling").value;}
+    else if(race == "gnome"){ subRace = document.getElementById("subGnome").value;}
     else{
         subRace = "none";
     }
+
     if(race == "" || chtrClass == "" || background == "" || subRace == ""){
         alert("Not all fields are filled out!");
     }
     else{
+        localStorage.setItem('race', race);
+        localStorage.setItem('subRace', subRace);
+        localStorage.setItem('chtrClass', chtrClass);
+        localStorage.setItem('background', background);
+        localStorage.setItem('level', level);
         window.location.href="html/proficiencies.html";
+    }
+}
+
+function chosenAttributes(){
+    var backProf = "You already have # as proficiencies from your " + getBackgroundString() + " background.";
+    var para = document.createElement("p");
+    var node = document.createTextNode(backProf);
+    para.appendChild(node);
+    var element = document.getElementById("div1");
+    element.appendChild(para);
+}
+
+function getBackgroundString(){
+    background = localStorage.getItem('background');
+    switch(background){
+        case "acolyte": return "Acolyte";
+        case "charlatan": return "Charlatan";
+        case "criminal": return "Criminal";
+        case "entertainer": return "Entertainer";
+        case "folkHero": return "Folk Hero";
+        case "artisan": return "Artisan";
+        case "hermit": return "Hermit";
+        case "noble": return "Noble";
+        case "outlander": return "Outlander";
+        case "sage": return "Sage";
+        case "sailor": return "Sailor";
+        case "soldier": return "Soldier";
+        case "urchin": return "Urchin";
+        default: break;
+    }
+}
+
+function getBackgroundProficiencies(){
+    background = localStorage.getItem('background');
+    switch(background){
+        case "acolyte": return "Acolyte";
+        case "charlatan": return "Charlatan";
+        case "criminal": return "Criminal";
+        case "entertainer": return "Entertainer";
+        case "folkHero": return "Folk Hero";
+        case "artisan": return "Artisan";
+        case "hermit": return "Hermit";
+        case "noble": return "Noble";
+        case "outlander": return "Outlander";
+        case "sage": return "Sage";
+        case "sailor": return "Sailor";
+        case "soldier": return "Soldier";
+        case "urchin": return "Urchin";
+        default: break;
     }
 }
