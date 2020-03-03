4d4e86f204a3801a376fb194eebbd6007e87bd05
Thomas Powell <tksp98@gmail.com>
2020-03-03 14:07:02 -0600

    Score modifiers update on input

--- src/js/abilityHP.js
+++ src/js/abilityHP.js
@@ -23,7 +23,18 @@ function chosenScores(){
     }
     var element = document.getElementById("div1");
     element.appendChild(para);
+
+    document.getElementById("div1").innerHTML = getLvl1HP();
     
+}
+
+function updateScoreMod(){
+    document.getElementById("strMod").innerHTML = getAbilityScore("str");
+    document.getElementById("dexMod").innerHTML = getAbilityScore("dex");
+    document.getElementById("conMod").innerHTML = getAbilityScore("con");
+    document.getElementById("intMod").innerHTML = getAbilityScore("int");
+    document.getElementById("wisMod").innerHTML = getAbilityScore("wis");
+    document.getElementById("chaMod").innerHTML = getAbilityScore("cha");
 }
 
 function raceScoreIncrease(){
@@ -51,7 +62,59 @@ function subRaceScoreIncrease(){
         case "lightHalfling": return "Lightfoot Halfling you also gain +1 to Charisma.";
         case "stoutHalfling": return "Stout Halfling you also gain +1 to Constitution.";
         case "forestGnome": return "Forest Gnome you also gain +1 to Dexterity.";
-        case "rockGnome": return "Rock Gnome toy also gain +1 to Constitution.";
+        case "rockGnome": return "Rock Gnome you also gain +1 to Constitution.";
+        default: break;
+    }
+}
+
+function getAbilityScore(ability){
+    switch(ability){
+        case "str": return getScoreModifier(document.getElementById("strScore").value);
+        case "dex": return getScoreModifier(document.getElementById("dexScore").value);
+        case "con": return getScoreModifier(document.getElementById("conScore").value);
+        case "int": return getScoreModifier(document.getElementById("intScore").value);
+        case "wis": return getScoreModifier(document.getElementById("wisScore").value);
+        case "cha": return getScoreModifier(document.getElementById("chaScore").value);
+        default: break;
+    }
+}
+
+function getScoreModifier(score){
+    switch(score){
+        case "1": return "-5";
+        case "2": case "3": return "-4";
+        case "4": case "5": return "-3";
+        case "6": case "7": return "-2";
+        case "8": case "9": return "-1";
+        case "10": case "11": return "+0";
+        case "12": case "13": return "+1";
+        case "14": case "15": return "+2";
+        case "16": case "17": return "+3";
+        case "18": case "19": return "+4";
+        case "20": case "21": return "+5";
+        case "22": case "23": return "+6";
+        case "24": case "25": return "+7";
+        case "26": case "27": return "+8";
+        case "28": case "29": return "+9";
+        case "30": return "+10";
+        default: return "";
+    }
+}
+
+function getLvl1HP(){
+    switch(chtrClass){
+        case "barbarian": return "As a barbarian, your level 1 HP is ";
+        case "bard": return 3;
+        case "cleric": return 2;
+        case "druid": return 2;
+        case "fighter": return 2;
+        case "monk": return 2;
+        case "paladin": return 2;
+        case "ranger": return 3;
+        case "rogue": return 4;
+        case "sorcerer": return 2;
+        case "warlock": return 2;
+        case "wizard": return 2;
         default: break;
     }
 }
